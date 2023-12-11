543f4d73ddf364b36d27796452b9f34fa7781b7b
refinmaldini <refin22@gmail.com>
2023-12-11 12:26:43 +0700

    add api

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import authRouters from "./routes/auth.js";
+import userRouters from "./routes/users.js";
+import postRouters from "./routes/posts.js";
+import cookieParser from 'cookie-parser';
+import multer from "multer"
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../client/public/upload')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now()+file.originalname)
+    }
+  })
+  
+  const upload = multer({ storage: storage })
+
+app.post('/api/upload', upload.single('file'), function(req,res) {
+    const file=req.file
+    res.status(200).json(file.filename)
+
+})
+
+app.use("/api/auth", authRouters)
+app.use("/api/user", userRouters)
+app.use("/api/posts", postRouters)
+
+app.listen(8800,()=> {
+    console.log("Connected!")
+})
